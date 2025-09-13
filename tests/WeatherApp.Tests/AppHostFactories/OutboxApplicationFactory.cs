b82b0ec702ee9e6286e9c3e72dcbb15730b68cb4
Andrew <andrewjpoole@gmail.com>
2025-09-13 11:47:55 +0100

    some test improvements

--- tests/WeatherApp.Tests/AppHostFactories/OutboxApplicationFactory.cs
+++ tests/WeatherApp.Tests/AppHostFactories/OutboxApplicationFactory.cs
@@ -21,8 +21,7 @@ public class OutboxApplicationFactory(ComponentTestFixture fixture) : WebApplica
     {        
         Environment.SetEnvironmentVariable("ConnectionStrings__WeatherAppDb", "dummyConnectionString");
         Environment.SetEnvironmentVariable(
-            $"{nameof(OutboxProcessorOptions)}__{nameof(OutboxProcessorOptions.IntervalBetweenBatchesInSeconds)}", 
-            "1");
+            $"{nameof(OutboxProcessorOptions)}__{nameof(OutboxProcessorOptions.IntervalBetweenBatchesInSeconds)}", "1");
 
         builder
             .ConfigureServices(services =>
--- tests/WeatherApp.Tests/ComponentTests.cs
+++ tests/WeatherApp.Tests/ComponentTests.cs
@@ -23,7 +23,7 @@ public class ComponentTests
     [Test]
     public void Return_a_WeatherReport_given_valid_region_and_date()
     {
-        var (given, when, then) = testFixture.SetupHelpers();
+        var (given, when, then, cannedData) = testFixture.SetupHelpers();
 
         given.WeHaveAWeatherReportRequest("bristol", DateTime.Now, out var apiRequest)
             .And.TheServersAreStarted();
@@ -51,7 +51,7 @@ public class ComponentTests
         then.And.TheModelingServiceSubmitEndpointShouldHaveBeenCalled(times: 1)
             .And.TheDomainEventShouldHaveBeenPersisted<SubmittedToModeling>()
             .And.TheResponseCodeShouldBe(response, HttpStatusCode.OK)
-            .And.TheBodyShouldNotBeEmpty<WeatherDataCollectionResponse>(response)
+            .And.TheBodyShouldNotBeEmpty(response)
             .And.WeGetTheStreamIdFromTheInitialDomainEvent(cannedData.RequestId, out var streamId);
         
         when.InPhase("2 (1st ASB message back from modeling service)")
@@ -66,7 +66,7 @@ public class ComponentTests
             .And.AnOutboxRecordWasInserted<UserNotificationEvent>();
 
         then.InPhase("4 (Notification Service handles event dispached by outbox)")
-            .AfterSomeTimeHasPassed(2_000, 1_000)
+            .AfterSomeTimeHasPassed(1_000, 500)
             .And.TheMessageWasHandled<ModelUpdatedIntegrationEvent>()
             .And.TheNotificationServiceNotifiedTheUser(cannedData.Location, cannedData.Reference);
     }
@@ -74,9 +74,7 @@ public class ComponentTests
     [Test]
     public void ApiPhase_EndsInConflict_IfExistingAggregateFound_WithSameRequestIdButDifferentReference()
     {
-        var (given, when, then) = testFixture.SetupHelpers();
-        var cannedData = new CannedData();
-
+        var (given, when, then, cannedData) = testFixture.SetupHelpers();
         given.WeHaveSomeCollectedWeatherData(cannedData, out var weatherData)
             .And.WeHaveResetEverything()
             .And.ThereIsExistingData(cannedData.UpTo_WeatherDataCollectionInitiated(reference: "differentReference"))
@@ -93,9 +91,7 @@ public class ComponentTests
     [Test]
     public void ApiPhase_EndsInSameFailure_IfExistingAggregateFound_WhichEndedInAPermanentFailure()
     {
-        var (given, when, then) = testFixture.SetupHelpers();
-        var cannedData = new CannedData();
-
+        var (given, when, then, cannedData) = testFixture.SetupHelpers();
         given.WeHaveSomeCollectedWeatherData(cannedData, out var weatherData)
             .And.WeHaveResetEverything()
             .And.ThereIsExistingData(cannedData.UpTo_WeatherModelingServiceRejectionFailure())
