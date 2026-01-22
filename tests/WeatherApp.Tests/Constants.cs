347fcb9800c5c669fdf24fd22df287841d05e22d
Andrew <andrewjpoole@gmail.com>
2026-01-22 08:45:38 +0000

    removed the old nunit component tests project

--- tests/WeatherApp.Tests/Constants.cs
+++
@@ -1,8 +0,0 @@
-namespace WeatherApp.Tests;
-
-public class Constants
-{
-    public const string WeatherModelingServiceSubmissionUri = "v1/collected-weather-data/";
-    public const string ContributorPaymentsServiceUriStart = "/v1/contributor-payments/";
-    public const string BaseUrl = "http://localhost/";
-}
